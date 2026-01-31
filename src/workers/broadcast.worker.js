38ec178531abeaa56cb188c913033f18b5897036
shivamgupta039057 <140205466+shivamgupta039057@users.noreply.github.com>
2026-01-31 20:18:19 +0530

    broadcast wor

--- src/workers/broadcast.worker.js
+++ src/workers/broadcast.worker.js
@@ -1,14 +1,13 @@
 const broadcastQueue = require("../helper/redis");
-require("dotenv").config(); 
+require("dotenv").config();
 const { sendTemplate } = require("../app/services/whatsappService");
-const { BroadcastLead, Broadcast,initDB } = require("../pgModels/index");
+const { BroadcastLead, Broadcast, initDB } = require("../pgModels/index");
 console.log("🚀 Broadcast Worker Started");
 
 (async () => {
   await initDB(); // 🔥 REQUIRED
   console.log("✅ Worker DB connected");
 })();
-
 
 // broadcastQueue.process(async (job) => {
 //   console.log("🔥 Job received:", job.data);
@@ -17,35 +16,32 @@ console.log("🚀 Broadcast Worker Started");
 //   return true;
 // });
 
-broadcastQueue.process(async job => {
-
+broadcastQueue.process(async (job) => {
   console.log("JOB RECEIVED:", job.data);
   const { broadcast_id, broadcast_lead_id } = job.data;
- const broadcastId = Number(broadcast_id);
-const leadId = Number(broadcast_lead_id);
-
+  const broadcastId = Number(broadcast_id);
+  const leadId = Number(broadcast_lead_id);
 
   const lead = await BroadcastLead.findByPk(leadId);
   const broadcast = await Broadcast.findOne({
-  where: { id: broadcastId }
-});
-
+    where: { id: broadcastId },
+  });
 
   try {
     await sendTemplate({
       phone: `91${lead.dataValues.phone}`,
-      template_name: broadcast.dataValues.template_name
+      template_name: broadcast.dataValues.template_name,
+      language : "en_US"
+
     });
 
     await lead.update({ status: "SENT" });
-
   } catch (err) {
-
-    console.log(err, "eeeeeeeeeeeeeeee")
+    console.log(err, "eeeeeeeeeeeeeeee");
     await lead.update({
       status: "FAILED",
       attempts: lead.attempts + 1,
-      error_message: err.message
+      error_message: err.message,
     });
 
     if (broadcast.auto_retry && lead.attempts < 3) {
